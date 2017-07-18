1877defa20cbce0548c7c3cbad0f06abd81605f7
tooshar <mr.tusharprajapati@gmail.com>
2017-07-18 15:36:04 +0530

    p5 files added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My heroku app' });
+  res.render('/views/Day 3 # Alien/empty-example/index');
 });
 
 module.exports = router;
